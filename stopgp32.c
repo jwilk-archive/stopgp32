jwilk-archive/stopgp32

commit e7a4469291d04add6db9e9d96ee64a29d3753ce5

Fix error handling.

stopgp32.c
@@ -109,12 +109,12 @@ static int get_cache_dir(void)
             posix_error("$HOME");
         }
         int size = snprintf(path, sizeof path, "%s/.cache", home);
+        if (size < 0)
+            posix_error("$HOME/.cache");
         if (size >= sizeof path) {
             errno = ENAMETOOLONG;
             size = -1;
         }
-        if (size < 0)
-            posix_error("$HOME/.cache");
     }
     int rc = mkdir(path, 0700);
     if (rc < 0 && errno != EEXIST)

commit 6281472d3bb555c685fef2c3b260ca017f578d25

Fix error handling for oversized $HOME.
Fixes: e7a4469291d04add6db9e9d96ee64a29d3753ce5

stopgp32.c
@@ -118,7 +118,7 @@ static int get_cache_dir(void)
             posix_error("$HOME/.cache");
         if (size >= sizeof path) {
             errno = ENAMETOOLONG;
-            size = -1;
+            posix_error("$HOME/.cache");
         }
     }
     int rc = mkdir(path, 0700);