jwilk-archive/stopgp32

commit 0bc5c9efd418c88090058aba83c055e8e833e28b

Unexpand $HOME even when $XDG_CACHE_HOME is set.

stopgp32.c
@@ -103,6 +103,7 @@ struct cache_dir
 
 static void cache_dir_init(struct cache_dir *o)
 {
+    const char *home = getenv("HOME");
     const char *cache_home = getenv("XDG_CACHE_HOME");
     if (cache_home && cache_home[0] != '/')
         cache_home = NULL;
@@ -116,7 +117,6 @@ static void cache_dir_init(struct cache_dir *o)
         }
         o->home_path = NULL;
     } else {
-        char *home = getenv("HOME");
         if (!home) {
             errno = ENOTDIR;
             posix_error("$HOME");
@@ -128,7 +128,11 @@ static void cache_dir_init(struct cache_dir *o)
             errno = ENAMETOOLONG;
             posix_error("$HOME/.cache/" PROGRAM_NAME);
         }
-        o->home_path = o->path + strlen(home) + 1;
+    }
+    if (home) {
+        size_t home_len = strlen(home);
+        if ((strncmp(o->path, home, home_len) == 0) && (o->path[home_len] == '/'))
+            o->home_path = o->path + home_len + 1;
     }
     char *p = strrchr(o->path, '/');
     assert(p != NULL);

commit 63ed826c5d300f66acd28bd88a87fa1a01ea494a

Fix uninitialized memory when $HOME isn't set.
Fixes: 0bc5c9efd418c88090058aba83c055e8e833e28b

stopgp32.c
@@ -145,6 +145,7 @@ static void cache_dir_init(struct cache_dir *o)
     const char *cache_home = getenv("XDG_CACHE_HOME");
     if (cache_home && cache_home[0] != '/')
         cache_home = NULL;
+    o->home_path = NULL;
     if (cache_home) {
         int size = snprintf(o->path, sizeof o->path, "%s/" PROGRAM_NAME, cache_home);
         if (size < 0)
@@ -153,7 +154,6 @@ static void cache_dir_init(struct cache_dir *o)
             errno = ENAMETOOLONG;
             posix_error("$XDG_CACHE_HOME/" PROGRAM_NAME);
         }
-        o->home_path = NULL;
     } else {
         if (!home) {
             errno = ENOTDIR;